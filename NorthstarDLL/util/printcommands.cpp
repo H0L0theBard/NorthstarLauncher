af939aa6a07e4870f3bc53e4dd37271fbe5969e6
H0L0theBard <H0L0.Business@outlook.com>
2023-09-04 14:05:36 +1000

    Update printcommands.cpp

--- NorthstarDLL/util/printcommands.cpp
+++ NorthstarDLL/util/printcommands.cpp
@@ -164,7 +164,7 @@ void ConCommand_findflags(const CCommand& arg)
 void ConCommand_list(const CCommand& arg)
 {	std::unordered_map map = R2::g_pCVar->DumpToMap();
 	std::vector<std::pair<std::string, ConCommandBase*> > sorted(map.begin(), map.end());
-	std::sort(sorted.begin(),sorted.end(),[](std::string& a, std::string& b) { return a > b; }));
+	std::sort(sorted.begin(),sorted.end(),[](std::string& a, std::string& b) { return a > b; });
 	for (auto& ConCommand : sorted)
 	{
 		PrintCommandHelpDialogue(ConCommand.second, ConCommand.second->m_pszName);
