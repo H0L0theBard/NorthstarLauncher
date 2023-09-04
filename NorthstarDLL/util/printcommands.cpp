03bb25f7c658bb4159b4aef0775e15876c73dcce
H0L0theBard <H0L0.Business@outlook.com>
2023-09-04 12:50:20 +1000

    Update printcommands.cpp

--- NorthstarDLL/util/printcommands.cpp
+++ NorthstarDLL/util/printcommands.cpp
@@ -174,10 +174,12 @@ void ConCommand_differences(const CCommand& arg)
 	for (auto& map : R2::g_pCVar->DumpToMap())
 	{
 		ConVar* cvar = R2::g_pCVar->FindVar(map.second->m_pszName);
-		if(cvar && strcmp(cvar->GetString(), cvar->m_pszDefaultValue) != NULL){
-			PrintCommandHelpDialogue(map.second, map.second->m_pszName);
-			spdlog::info("Value: {}", cvar->m_Value.m_pszString);
-			spdlog::info("Default: {}", cvar->m_pszDefaultValue);
+		if(cvar && strcmp(cvar->GetString(), "FCVAR_NEVER_AS_STRING") != NULL){
+			if(strcmp(cvar->GetString(), cvar->m_pszDefaultValue) != NULL){
+				PrintCommandHelpDialogue(map.second, map.second->m_pszName);
+				spdlog::info("Value: {}", cvar->m_Value.m_pszString);
+				spdlog::info("Default: {}", cvar->m_pszDefaultValue);
+			}
 		}
 	}
 }
